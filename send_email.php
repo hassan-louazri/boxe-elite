9a737490b04aa1fb8d35802b964a81472d7ed264
hassan <hassanlouazri@outlook.com>
2024-07-13 19:45:13 +0200

    php messages en français

--- send_email.php
+++ send_email.php
@@ -16,14 +16,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $son = false;
     // NAME
     if (empty($name) || empty($email) || empty($subject) || empty($content )) {
-       $errors_list[] ="Inputs are required";
+       $errors_list[] ="Tous les champs sont requis.";
     }
     if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-        $errors_list[] = "Only letters and white space allowed";
+        $errors_list[] = "Lettres et espace seulement pour votre nom.";
     }
     // validate email
     if (!is_valid_email($email)) {
-        $errors_list[] =  "Invalid Email Format";
+        $errors_list[] =  "Email invalide.";
     }else{
         // Email Exists
         $to = 'contact@eliteboxing.fr';
@@ -34,17 +34,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (mail($to, $subject, $content, $headers)) {
             $son = true;
         } else {
-            $errors_list[] = "Sorry, something went wrong. Please try again later.";
+            $errors_list[] = "Quelque chose ne marche pas. Veuillez réessayer ulterieurement. Vous pouvez nous contacter directement à travers notre courriel électronique.";
         }
     }
     
     echo json_encode(['request'=>"sent successfully",
                       'errors' =>$errors_list,
                       'email_sent'=>$son]);
-    exit();
-
-
-    
+    exit();    
 } else {
    
     echo json_encode(['error'=>"Invalid request method."]);
